f028ddbaf2425739148ce4e137ef4d7135965797
edrasen <alain42117@hotmail.com>
2019-08-26 23:13:10 -0500

    agregando nuevo repositoio

---
+++ login/script.js
@@ -0,0 +1,18 @@
+function myfunction(){
+	var usr = document.getElementById('usr').value;
+	var pass = document.getElementById('pass').value;
+	validar(usr, pass);
+}
+
+function validar(usr, pass){
+	if (usr == "2013090243" && pass == "edgar") 
+	{
+		alert("Bienvenido");
+		document.getElementById('usr').value='';
+		document.getElementById('pass').value='';
+	}
+	else
+	{
+		alert("Datos incorrectos")
+	}
+}
